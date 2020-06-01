ad02bf040492d858ec5d28e015f75648f9b86df7
ize3 <upem.israel.arias@gmail.com>
2020-05-31 22:12:41 -0500

    Add:all proyect

---
+++ src/proyectomichel/Proyectomichel.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectomichel;
+
+/**
+ *
+ * @author iz3e_
+ */
+public class Proyectomichel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
