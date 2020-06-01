b12b32c00ec5a4677693939a73334d7abb23c2d6
ize3 <upem.israel.arias@gmail.com>
2020-05-31 22:15:19 -0500

    Add:hola mundo

--- src/proyectomichel/Proyectomichel.java
+++ src/proyectomichel/Proyectomichel.java
@@ -15,7 +15,8 @@ public class Proyectomichel {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code appli8cation logic here
+        System.out.println("Hola mundo");
     }
     
 }
